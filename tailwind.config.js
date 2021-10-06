a66f82c7cd7b0cf57959b475d5a7d7ac1641bb4a
Nirmal Thomas Mathew <55346160+nirmal15mathew@users.noreply.github.com>
2021-10-06 15:05:01 +0530

    fixed dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
      "./src/**/*.svelte",
 
     ],
-    enabled: production // disable purge in dev
+    enabled: production // disable purge in dev,
+    options: {
+      safelist: ["dark"]
+    }
   },
   darkMode: 'class', // or 'media' or 'class'
   theme: {
