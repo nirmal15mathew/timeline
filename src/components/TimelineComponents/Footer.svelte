dd942de1609c026d9fd88dd6b7c03e78f367e00f
Nirmal Thomas Mathew <55346160+nirmal15mathew@users.noreply.github.com>
2021-10-11 09:42:53 +0530

    added keyboard control for entering events

--- src/components/TimelineComponents/Footer.svelte
+++ src/components/TimelineComponents/Footer.svelte
@@ -8,9 +8,12 @@
         dispatch('add-event', inputBuffer)
         inputBuffer = ""
     }
+    const onKeyPress = e => {
+        if (e.keyCode === 13) handleClick()
+    }
 </script>
 <footer class="bg-gray-200 w-full p-3 flex dark:bg-gray-700">
-    <input type="text" class="rounded-lg p-2 outline-none dark:bg-gray-400 dark:focus:bg-gray-100 text-gray-800 flex-grow focus:ring focus:ring-gray-400" placeholder="Add new event" bind:value={inputBuffer}>
+    <input type="text" class="rounded-lg p-2 outline-none dark:bg-gray-400 dark:focus:bg-gray-100 text-gray-800 flex-grow focus:ring focus:ring-gray-400" placeholder="Add new event" bind:value={inputBuffer} on:keypress={onKeyPress}>
     <button class="p-2 rounded-lg bg-indigo-500 ml-3 text-gray-50 flex-none flex justify-center h-10 w-10" on:click={handleClick}>
         <PlusCircleIcon size="1.5x"/>
     </button>
